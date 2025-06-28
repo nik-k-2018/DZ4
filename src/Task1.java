87143aaf97907d4241fce9e58c73cb7bad3835c3
Azerty <nik.k.2018@mail.ru>
2025-06-28 16:30:08 +0300

    Добавлена задача 1: выбор ОС

---
+++ src/Task1.java
@@ -0,0 +1,13 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int clientOS = 0; // 0 — iOS, 1 — Android
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Неизвестная операционная система");
+        }
+    }
+}
