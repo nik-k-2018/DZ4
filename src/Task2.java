d80a079dee2faaf8c14084c280d98e910b28114c
Azerty <nik.k.2018@mail.ru>
2025-06-28 16:35:24 +0300

    Добавлена задача 2: учёт года устройства

---
+++ src/Task2.java
@@ -0,0 +1,22 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int clientOS = 0; // 0 — iOS, 1 — Android
+        int clientDeviceYear = 2013; // Год выпуска телефона
+
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Неизвестная операционная система");
+        }
+    }
+}
